0fcdbd341ac684c42b4980e3639fbe5b40da9749
guinetik <guinetik@gmail.com>
2025-05-03 13:17:05 -0300

    update build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 ﻿# a rudimentary build scripts. fixes encoding and installs the module opening a new powershell instance for the tests.
 & ./fix-encoding.ps1
 & ./install.ps1
-& all my shell
+& all my terminal
