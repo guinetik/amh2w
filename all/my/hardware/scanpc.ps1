3b94db2812c02a1269a36c38ff02559bbc673bc8
guinetik <guinetik@gmail.com>
2025-05-04 06:05:40 -0300

    New 'all my hardware scanpc' command to execute all subcommands under hardware

---
+++ all/my/hardware/scanpc.ps1
@@ -0,0 +1,22 @@
+﻿function scanpc {
+    try {
+        & all my hardware bios
+        & all my hardware motherboard
+        & all my hardware cpu
+        & all my hardware gpu
+        & all my hardware ram
+        & all my hardware power
+        & all my hardware storage
+        & all my hardware bluetooth
+        & all my network internet
+        & all my network dns check
+        & all my network ip interfaces
+        & all my homies hate windows version
+        return Ok "PC scanned successfully"
+    }
+    catch {
+        Log-Error "Error scanning PC: $_"
+        return Err "Error scanning PC: $_"
+    }
+
+}
